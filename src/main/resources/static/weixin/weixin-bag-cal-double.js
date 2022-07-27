b6498820a768712052d91a6c6349cf8c777a160e
zsl131 <398986099@qq.com>
2022-07-27 16:54:03 +0800

    充值3倍

--- src/main/resources/static/weixin/weixin-bag-cal-double.js
+++ src/main/resources/static/weixin/weixin-bag-cal-double.js
@@ -4,8 +4,8 @@ function onCal() {
             '</div><div class="form-group form-group-lg">'+
             '</div>'+
             '<div class="row">'+
-            '<div class="col-md-6 col-sm-6 col-xs-6"><p>充<b>三倍</b>享半价：</p><div class="charge2"><p>充值金额：<b style="color:#F00" class="value1">0</b></p><p>餐费金额：<b style="color:#F00" class="value2">0</b></p><p>合计需支付：<b style="color:#F00" class="value3">0</b></p><p>到账金额：<b style="color:#F00" class="value4">0</b></p></div></div>'+
-            '<div class="col-md-6 col-sm-6 col-xs-6"><p>充<b>四倍</b>享免费：</p><div class="charge4"><p>充值金额：<b style="color:#F00" class="value1">0</b></p><p>餐费金额：<b style="color:#F00" class="value2">0</b></p><p>合计需支付：<b style="color:#F00" class="value3">0</b></p><p>到账金额：<b style="color:#F00" class="value4">0</b></p></div></div>'+
+            '<div class="col-md-6 col-sm-6 col-xs-6"><p>充<b>两倍</b>享半价：</p><div class="charge2"><p>充值金额：<b style="color:#F00" class="value1">0</b></p><p>餐费金额：<b style="color:#F00" class="value2">0</b></p><p>合计需支付：<b style="color:#F00" class="value3">0</b></p></div></div>'+
+            '<div class="col-md-6 col-sm-6 col-xs-6"><p>充<b>四倍</b>享免费：</p><div class="charge4"><p>充值金额：<b style="color:#F00" class="value1">0</b></p><p>餐费金额：<b style="color:#F00" class="value2">0</b></p><p>合计需支付：<b style="color:#F00" class="value3">0</b></p></div></div>'+
             '</div>'+
             '<div>注意：消费金额<span style="color:#F00">不包含押金</span>等金额</div>'+
             '</div>';
@@ -17,15 +17,13 @@ function onCal() {
 function changeMoney(obj) {
     var money = parseFloat($(obj).val());
     var obj2 = $(obj).parents(".charge-div").find(".charge2");
-    $(obj2).find(".value1").html((money*3).toFixed(2));
+    $(obj2).find(".value1").html((money*2).toFixed(2));
     $(obj2).find(".value2").html((money/2).toFixed(2));
-    $(obj2).find(".value3").html((money*3).toFixed(2));
-    $(obj2).find(".value4").html((money*2.5).toFixed(2));
+    $(obj2).find(".value3").html((money*2.5).toFixed(2));
 
     var obj4 = $(obj).parents(".charge-div").find(".charge4");
     $(obj4).find(".value1").html((money*4).toFixed(2));
     $(obj4).find(".value2").html(0);
     $(obj4).find(".value3").html((money*4).toFixed(2));
-    $(obj4).find(".value4").html((money*4).toFixed(2));
     //console.log(money)
 }
